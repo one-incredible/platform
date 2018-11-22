42d31902e430f2bc8af87c2073cd9054cd77aeef
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-22 19:08:19 +0100

    Revert "Skip fields clause."
    This reverts commit ab545b0f1aec7ea707e59224bcaa432b5ab1f29f.

--- packages/backend/src/models/query.js
+++ packages/backend/src/models/query.js
@@ -2,8 +2,10 @@ export function createFetchRevision(Model, name) {
   const parentTable = name;
   const revisionTable = `${name}_revision`;
 
+  const fields = Model.fields.map(field => `r.${field.name}`);
+
   const text = [
-    'SELECT r.*',
+    'SELECT ' + fields.join(', '),
     `FROM ${revisionTable} r`,
     `JOIN ${parentTable} p ON p.id = r.id AND p.revision = r.revision`,
     'WHERE p.id = $1',
