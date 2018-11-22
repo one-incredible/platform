720c15b95b16df1feddaf0dca190520f0c8f88e0
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-22 10:56:24 +0100

    Dynamically create revision store query.

--- packages/backend/src/models/__tests__/query.test.js
+++ packages/backend/src/models/__tests__/query.test.js
@@ -1,17 +1,45 @@
 import { createModel, field } from '../model';
-import { createFetchRevision } from '../query';
+import { createFetchRevision, createStoreRevision } from '../query';
 
 describe('Query modules', () => {
-  describe('#createFetchRevision', () => {
-    const Model = createModel([field('name'), field('length'), field('rate')]);
+  const Model = createModel([
+    field('id'),
+    field('name'),
+    field('length'),
+    field('rate'),
+  ]);
 
+  describe('#createFetchRevision', () => {
     it('creates a query builder based on model', () => {
       const createQuery = createFetchRevision(Model, 'video');
       const query = createQuery('my-whatever-id');
       expect(query.text).toEqual(
-        'SELECT r.name, r.length, r.rate FROM video_revision r JOIN video p ON p.id = r.id AND p.revision = r.revision WHERE p.id = $1'
+        'SELECT r.id, r.name, r.length, r.rate FROM video_revision r JOIN video p ON p.id = r.id AND p.revision = r.revision WHERE p.id = $1'
       );
       expect(query.values).toEqual(['my-whatever-id']);
+    });
+  });
+
+  describe('#createStoreRevision', () => {
+    it('creates a query builder based on model', () => {
+      const model = Model.decode({
+        id: '352b2178-ee3c-11e8-9af5-00090ffe0001',
+        name: 'Barry',
+        length: 230.23,
+        rate: 12,
+      });
+
+      const createQuery = createStoreRevision(Model, 'video');
+      const query = createQuery(model);
+      expect(query.text).toEqual(
+        'INSERT INTO video_revision (id, name, length, rate, revision) SELECT $1, $2, $3, $4, COALESCE(MAX(revision) + 1, 1) FROM video_revision WHERE id = $1 RETURNING revision'
+      );
+      expect(query.values).toEqual([
+        '352b2178-ee3c-11e8-9af5-00090ffe0001',
+        'Barry',
+        230.23,
+        12,
+      ]);
     });
   });
 });
