5359ce4d385af279d5c38f74a2a4cdc92c64e8f5
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-22 11:23:15 +0100

    Move StorageAdapter creation to Storage module.

--- packages/backend/src/models/storage.js
+++ packages/backend/src/models/storage.js
@@ -1,3 +1,50 @@
+import {
+  createPromoteRevision,
+  createRevokeRevision,
+  createFetchRevision,
+  createStoreRevision,
+} from './query';
+
+export function createRevisionedStorageAdapter(Model, tableName) {
+  const Query = {
+    fetchRevision: createFetchRevision(Model, tableName),
+    storeRevision: createStoreRevision(Model, tableName),
+    revokeRevision: createRevokeRevision(tableName),
+    promoteRevision: createPromoteRevision(tableName),
+  };
+
+  class StorageAdapter extends Storage {}
+
+  StorageAdapter.prototype.fetch = async function fetch(modelId) {
+    const result = await this.db.query(Query.fetchRevision(modelId));
+    if (result.rowCount === 0) {
+      return null;
+    }
+
+    return Model.decode(result.rows[0]);
+  };
+
+  StorageAdapter.prototype.store = async function store(model) {
+    try {
+      await this.db.query('BEGIN');
+      await this.db.query(Query.revokeRevision(model));
+
+      const result = await this.db.query(Query.storeRevision(model));
+
+      const { revision } = result.rows[0];
+
+      await this.db.query(Query.promoteRevision(model, revision));
+
+      await this.db.query('COMMIT');
+    } catch (e) {
+      await this.db.query('ROLLBACK');
+      throw e;
+    }
+  };
+
+  return StorageAdapter;
+}
+
 export class Storage {
   constructor(db) {
     this.db = db;
