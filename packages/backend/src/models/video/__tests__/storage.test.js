1857de6eb5b2e0bd8dec4d601ee62f050fb02939
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-23 00:06:54 +0100

    Add Stream relation to Video object.

--- packages/backend/src/models/video/__tests__/storage.test.js
+++ packages/backend/src/models/video/__tests__/storage.test.js
@@ -1,13 +1,35 @@
 const uuidv4 = require('uuid/v4');
 const { Pool } = require('pg');
+const { Stream } = require('../../stream/model');
 const { Video } = require('../model');
+const { StreamStorage } = require('../../stream/storage');
 const { VideoStorage } = require('../storage');
 
 describe('Video Storage', () => {
-  let storage, db;
+  let storage, db, fixtureStreams;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     db = new Pool();
+    const streamStorage = new StreamStorage(db);
+    fixtureStreams = [1, 2, 3, 4].map(() =>
+      Stream.decode({
+        id: uuidv4(),
+        resolution: {
+          id: uuidv4(),
+          width: '2048',
+          height: '1024',
+        },
+        file: {
+          id: uuidv4(),
+          path: 'path/to/file',
+          mime: 'video/mp4',
+          size: 1241125,
+        },
+      })
+    );
+    await Promise.all(
+      fixtureStreams.map(stream => streamStorage.store(stream))
+    );
   });
 
   beforeEach(() => {
@@ -49,6 +71,32 @@ describe('Video Storage', () => {
       it('returns video for id', async () => {
         const returnedVideo = await storage.fetch(fixtureVideo.id);
         expect(returnedVideo).toEqual(fixtureVideo);
+      });
+
+      describe('and streams attached', () => {
+        beforeEach(async () => {
+          await storage.addStream(fixtureVideo.id, fixtureStreams[0].id);
+          await storage.addStream(fixtureVideo.id, fixtureStreams[2].id);
+        });
+
+        it('stream is avaiable on video', async () => {
+          const returnedVideo = await storage.fetch(fixtureVideo.id);
+          expect(returnedVideo.streams.length).toEqual(2);
+          expect(returnedVideo.streams[0]).toEqual(fixtureStreams[0]);
+          expect(returnedVideo.streams[1]).toEqual(fixtureStreams[2]);
+        });
+
+        describe('then stream detached', () => {
+          beforeEach(async () => {
+            await storage.removeStream(fixtureVideo.id, fixtureStreams[0].id);
+          });
+
+          it('stream is removed from video', async () => {
+            const returnedVideo = await storage.fetch(fixtureVideo.id);
+            expect(returnedVideo.streams.length).toEqual(1);
+            expect(returnedVideo.streams[0]).toEqual(fixtureStreams[2]);
+          });
+        });
       });
     });
   });
