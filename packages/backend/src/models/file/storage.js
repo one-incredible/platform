2dc015a3ca9b7852cb002a44e3d826a1f8cc73d1
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 21:33:26 +0100

    Skip decode in StorageAdapters.

--- packages/backend/src/models/file/storage.js
+++ packages/backend/src/models/file/storage.js
@@ -1,11 +1,7 @@
 const { createRevisionedStorageAdapter } = require('../storage');
 const { File } = require('./model');
 
-class FileStorage extends createRevisionedStorageAdapter(File, 'file') {
-  async fetch(fileId) {
-    return await super.fetch(fileId, result => File.decode(result));
-  }
-}
+const FileStorage = createRevisionedStorageAdapter(File, 'file');
 
 module.exports = {
   FileStorage,
