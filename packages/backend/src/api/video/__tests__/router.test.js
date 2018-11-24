c36265bd5422d35e22aca34db9f5273a354c5546
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 16:12:49 +0100

    WIP Video API.

---
+++ packages/backend/src/api/video/__tests__/router.test.js
@@ -0,0 +1,108 @@
+const uuidv4 = require('uuid/v4');
+const request = require('supertest');
+const app = require('app');
+
+function createStream() {
+  return {
+    id: uuidv4(),
+    resolution: {
+      id: uuidv4(),
+      width: 1920,
+      height: 1080,
+    },
+    file: {
+      id: uuidv4(),
+      path: 'path/to/file',
+      mime: 'video/mp4',
+      size: 1241125,
+    },
+  };
+}
+
+describe('Stream API', () => {
+  describe('GET /', () => {
+    describe('with non-existing id', () => {
+      it('responds with 404', done => {
+        request(app)
+          .get('/api/stream/b1354cdc-efc5-11e8-9804-00090ffe0001')
+          .expect(404)
+          .expect('')
+          .end(done);
+      });
+    });
+
+    describe('with existing id', () => {
+      let stream;
+
+      beforeEach(done => {
+        stream = createStream();
+
+        request(app)
+          .post('/api/stream/')
+          .send(stream)
+          .expect(201)
+          .end(done);
+      });
+
+      it('responds with 200 and content', done => {
+        request(app)
+          .get(`/api/stream/${stream.id}`)
+          .expect(200)
+          .expect(JSON.stringify(stream))
+          .end(done);
+      });
+    });
+  });
+
+  describe('POST /', () => {
+    let stream;
+
+    beforeEach(() => {
+      stream = createStream();
+    });
+
+    describe('with non-existing id', () => {
+      beforeEach(done => {
+        request(app)
+          .post('/api/stream/')
+          .send(stream)
+          .expect(201)
+          .expect('location', `/api/stream/${stream.id}`)
+          .end(done);
+      });
+
+      it('can be retrieved', done => {
+        request(app)
+          .get(`/api/stream/${stream.id}`)
+          .expect(200)
+          .expect(JSON.stringify(stream))
+          .end(done);
+      });
+
+      describe('then with existing id', () => {
+        beforeEach(done => {
+          stream.resolution = {
+            id: uuidv4(),
+            width: 3840,
+            height: 2160,
+          };
+
+          request(app)
+            .post('/api/stream/')
+            .send(stream)
+            .expect(201)
+            .expect('location', `/api/stream/${stream.id}`)
+            .end(done);
+        });
+
+        it('has been updated', done => {
+          request(app)
+            .get(`/api/stream/${stream.id}`)
+            .expect(200)
+            .expect(JSON.stringify(stream))
+            .end(done);
+        });
+      });
+    });
+  });
+});
