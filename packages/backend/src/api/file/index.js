16f2d744304e78d67fcb1d49e82de825901371f5
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 10:04:12 +0100

    Decouple Router code.

--- packages/backend/src/api/file/index.js
+++ packages/backend/src/api/file/index.js
@@ -1,29 +1,6 @@
-const express = require('express');
 const { File } = require('models/file/model');
 const { FileStorage } = require('models/file/storage');
-
-function createStorageRouter(Model, storage) {
-  const router = express.Router();
-
-  router.post('/', async (req, res) => {
-    const model = Model.decode(req.body);
-    await storage.store(model);
-    res.statusCode = 201;
-    res.set('location', `${req.baseUrl}/${model.id}`);
-    res.end();
-  });
-
-  router.get('/:modelId', async (req, res) => {
-    const result = await storage.fetch(req.params.modelId);
-    if (!result) {
-      res.statusCode = 404;
-      return res.end();
-    }
-    res.send(Model.encode(result));
-  });
-
-  return router;
-}
+const { createStorageRouter } = require('api/router');
 
 function createFileAPIRouter(db) {
   const storage = new FileStorage(db);
