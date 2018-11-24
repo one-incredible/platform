06a25c0896d9c31650efb60337742dc961019590
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 10:02:02 +0100

    Add test.

--- packages/backend/src/__tests__/app.test.js
+++ packages/backend/src/__tests__/app.test.js
@@ -16,30 +16,94 @@ describe('App', () => {
 
     describe('/file', () => {
       describe('GET /', () => {
-        it('responds with Hello file', done => {
-          request(app)
-            .get('/api/file/')
-            .expect(200)
-            .expect('Hello file null')
-            .end(done);
+        describe('with non-existing id', () => {
+          it('responds with 404', done => {
+            request(app)
+              .get('/api/file/b1354cdc-efc5-11e8-9804-00090ffe0001')
+              .expect(404)
+              .expect('')
+              .end(done);
+          });
+        });
+
+        describe('with existing id', () => {
+          let file;
+
+          beforeEach(done => {
+            file = {
+              id: uuidv4(),
+              path: 'my/file/here.mp4',
+              mime: 'video/mp4',
+              size: 21152125,
+            };
+
+            request(app)
+              .post('/api/file/')
+              .send(file)
+              .expect(201)
+              .end(done);
+          });
+
+          it('responds with 200 and content', done => {
+            request(app)
+              .get(`/api/file/${file.id}`)
+              .expect(200)
+              .expect(JSON.stringify(file))
+              .end(done);
+          });
         });
       });
 
       describe('POST /', () => {
-        it('responds', done => {
-          const file = {
+        let file;
+
+        beforeEach(() => {
+          file = {
             id: uuidv4(),
             path: 'my/file/here.mp4',
             mime: 'video/mp4',
             size: 21152125,
           };
+        });
 
-          request(app)
-            .post('/api/file/')
-            .send(file)
-            .expect(201)
-            .expect('location', `/api/file/${file.id}`)
-            .end(done);
+        describe('with non-existing id', () => {
+          beforeEach(done => {
+            request(app)
+              .post('/api/file/')
+              .send(file)
+              .expect(201)
+              .expect('location', `/api/file/${file.id}`)
+              .end(done);
+          });
+
+          it('can be retrieved', done => {
+            request(app)
+              .get(`/api/file/${file.id}`)
+              .expect(200)
+              .expect(JSON.stringify(file))
+              .end(done);
+          });
+
+          describe('then with existing id', () => {
+            beforeEach(done => {
+              file.size = 600000;
+
+              request(app)
+                .post('/api/file/')
+                .send(file)
+                .expect(201)
+                .expect('location', `/api/file/${file.id}`)
+                .end(done);
+            });
+
+            it('has been updated', done => {
+              request(app)
+                .get(`/api/file/${file.id}`)
+                .expect(200)
+                .expect(JSON.stringify(file))
+                .end(done);
+            });
+          });
         });
       });
     });
