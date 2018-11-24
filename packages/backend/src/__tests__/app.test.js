2ca1ce9343ff0bb87c64cc4c9a264dbcf6244d6d
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 09:19:51 +0100

    Inject DB connection into API.

--- packages/backend/src/__tests__/app.test.js
+++ packages/backend/src/__tests__/app.test.js
@@ -2,11 +2,27 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', () => {
-  it('responds to /api', done => {
-    request(app)
-      .get('/api')
-      .expect(200)
-      .expect('Hello world')
-      .end(done);
+  describe('/api', () => {
+    describe('GET /', () => {
+      it('responds with Hello world', done => {
+        request(app)
+          .get('/api')
+          .expect(200)
+          .expect('Hello world')
+          .end(done);
+      });
+    });
+
+    describe('/file', () => {
+      describe('GET /', () => {
+        it('responds with Hello file', done => {
+          request(app)
+            .get('/api/file/')
+            .expect(200)
+            .expect('Hello file null')
+            .end(done);
+        });
+      });
+    });
   });
 });
--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { Pool } = require('pg');
 const { createAPI } = require('./api');
 
+const db = new Pool();
 const app = express();
 
-app.use('/api', createAPI());
+app.use('/api', createAPI(db));
 
 module.exports = app;
