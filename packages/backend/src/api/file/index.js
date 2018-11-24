3e23266e40ebc9ca3c2b199b5c283c7c36e2b788
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-24 09:02:37 +0100

    Create File API.

---
+++ packages/backend/src/api/file/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+//const { FileStorage } = require('models/file/storage');
+
+function createFileAPIRouter() {
+  //const storage = new FileStorage(db);
+
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+    res.send('Hello file');
+  });
+
+  return router;
+}
+
+module.exports = {
+  createFileAPIRouter,
+};
--- packages/backend/src/api/index.js
+++ packages/backend/src/api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { createFileAPIRouter } = require('./file');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hello world');
 });
+
+router.use('/file', createFileAPIRouter({}));
 
 module.exports = router;
