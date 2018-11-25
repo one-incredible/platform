d3bb99fed05e560c60a2cea19166ca03b8805ff0
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-25 20:49:32 +0100

    Move external deps to top.

--- packages/backend/src/api/stream/router.js
+++ packages/backend/src/api/stream/router.js
@@ -1,6 +1,6 @@
+const { createStorageRouter } = require('@oneinc/super-api/api/router');
 const { Stream } = require('models/stream/model');
 const { StreamStorage } = require('models/stream/storage');
-const { createStorageRouter } = require('@oneinc/super-api/api/router');
 
 function createStreamAPIRouter(db) {
   const storage = new StreamStorage(db);
